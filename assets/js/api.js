91a97c4a3c95e76beb9e84c0349ec1fe20a3f3ac
Katha leelavongpanich <ball.mdr@gmail.com>
2018-09-21 00:03:18 +0700

    Fix Ep carousel

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,6 +3,6 @@ import jsonapiParse from 'jsonapi-parse'
 const prefix = '/jsonapi'
 
 export async function getSerieByTitle (title) {
-  const { data } = await apiClient.get(prefix + '/series?filter[title]=' + title + '&include=field_poster,field_celeb,field_celeb.field_celeb_profile,field_episode_series,field_episode_series.field_thumbnail,field_other_img')
+  const { data } = await apiClient.get(prefix + '/series?filter[title]=' + title + '&include=field_poster,field_celeb,field_celeb.field_celeb_profile,field_episode_series,field_episode_series.field_thumbnail,field_episode_series.field_img_streaming,field_other_img')
   return jsonapiParse.parse(data).data
 }
