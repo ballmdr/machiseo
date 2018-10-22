cd5c2e92b2eaf5e7205e83846175565b35f29850
Ballmdr <ball.mdr@gmail.com>
2018-10-22 16:40:52 +0700

    browse alpah finish

--- pages/series/index/type/_type.vue
+++ pages/series/index/type/_type.vue
@@ -12,10 +12,11 @@
       <serie-card-group :series="series"></serie-card-group>
     </v-flex>
     <v-flex xs12 class="text-xs-center">
-      <v-progress-circular
+      <v-progress-circular v-if="!empty"
         indeterminate
         color="amber"
       ></v-progress-circular>
+      <v-icon v-else>remove_circle_outline</v-icon>
     </v-flex>
   </v-layout>
 </template>
@@ -25,27 +26,36 @@ import { getSeriesByType } from '~/assets/js/api'
 import SerieCardGroup from '~/components/series/SerieCardGroup'
 
 export default {
-  components: { SerieCardGroup },
+  components: { SerieCardGroup },  
   mounted() {
     window.onscroll = () => {
       let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight
       if (bottomOfWindow) {
-        getSeriesByType(this.offset, this.limit, this.type).then(newSeries => {
-          this.offset += 10
-          for (let i=0;i<newSeries.length;i++) {
-            this.series.push(newSeries[i])
-          }
-        })        
+        if (!this.empty) {
+          getSeriesByType(this.offset, this.limit, this.type).then(newSeries => {
+            this.offset += 9
+            if (newSeries.length < this.limit) {
+              this.empty = true
+            }
+            for (let i=0;i<newSeries.length;i++) {
+              this.series.push(newSeries[i])
+            }
+          })      
+        }    
       }
     }
   },
   async asyncData({ params }) {
     let offset = 0
-    let limit = 10
+    let limit = 9
+    let empty = false
     const type = params.type
     const series = await getSeriesByType(offset, limit, type)
-    offset += 10
-    return { offset, limit, type, series }
+    offset += 9
+        if (series.length < limit) {
+      empty = true
+    }
+    return { empty, offset, limit, type, series }
   }
 }
 </script>
