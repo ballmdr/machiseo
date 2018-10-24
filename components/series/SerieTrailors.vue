31dc9b3069aef024290edd339e873cfd970a61be
Ballmdr <ball.mdr@gmail.com>
2018-10-24 09:37:25 +0700

    trailor

---
+++ components/series/SerieTrailors.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-layout row wrap>
+    <v-flex d-flex class="hvr-float" style="cursor:pointer" xs5 sm3 v-for="(id, index) in youtubeId" :key="index" @click="openTrailor(index)">
+      <v-img style="width:100%" :src="'https://img.youtube.com/vi/' + id + '/default.jpg'"></v-img>
+    </v-flex>
+    <v-dialog v-model="trailorDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          ตัวอย่างซีรีส์​
+        </v-card-title>
+        <v-card-text>
+          <iframe width="100%" height="100%" :src="'//www.youtube.com/embed/' + currentId" frameborder="0" allowfullscreen></iframe>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" flat @click="trailorDialog = false">ปิด</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+import { getYoutubeId } from '~/assets/js/util'
+
+export default {
+  props: ['trailors'],
+  data () {
+    return {
+      youtubeId: [],
+      trailorDialog: false,
+      currentId: null
+    }
+  },
+  methods: {
+    openTrailor(index) {
+      this.currentId = this.youtubeId[index]
+      this.trailorDialog = true
+    }
+  },
+  mounted() {
+    for (let i=0;i < this.trailors.length;i++) {
+      this.youtubeId[i] = getYoutubeId(this.trailors[i])
+    }
+    console.log(this.youtubeId)
+  }
+}
+</script>
--- pages/series/_title.vue
+++ pages/series/_title.vue
@@ -4,28 +4,30 @@
       <div class="container">
         <v-card color="primary" class="u-clearfix">
           <v-layout row wrap>
-            <v-flex xs4 d-flex>
+            <v-flex xs12 sm6 md4>
               <v-card-text class="card-media">
                 <v-img :src="baseUrl + serie.field_poster[0].url" class="card-media-img"></v-img>
               </v-card-text>
             </v-flex>
-            <v-flex xs7>
+            <v-flex xs12 sm6 md8>
               <v-card-title>
                 <h1 class="display-2 font-weight-bold">{{ serie.title }}</h1>
               </v-card-title>
               <v-btn small nuxt :to="'/series/type/' + type.name" round color="warning" v-for="type in serie.field_series_type" :key="type.id" style="color:black">{{ type.name }}</v-btn>
               |
-              <v-btn small flat nuxt :to="'/series/channel/' + serie.field_channel.name" style="padding:0; margin:0">{{ serie.field_channel.name }}</v-btn>
-              <v-btn small flat nuxt :to="'/series/year/' + serie.field_serie_year.name" style="padding:0; margin:0">{{ serie.field_serie_year.name }}</v-btn>
+              <v-btn small nuxt :to="'/series/channel/' + serie.field_channel.name" style="padding:0; margin:0">{{ serie.field_channel.name }}</v-btn>
+              <v-btn small nuxt :to="'/series/year/' + serie.field_serie_year.name" style="padding:0; margin:0">{{ serie.field_serie_year.name }}</v-btn>
               <v-divider dark></v-divider>
               <v-card-text>
                 <p v-html="serie.body.processed"></p>
               </v-card-text>
             </v-flex>
+            <v-flex xs12 v-if="serie.field_trailor !== null">
+              <serie-trailors :trailors="serie.field_trailor"></serie-trailors>
+            </v-flex>
           </v-layout>
         </v-card>
       </div>
-      <v-spacer></v-spacer>
     </v-flex>
     <v-flex xs12>
       <v-layout row wrap justify-start>
@@ -52,9 +54,10 @@ import EpisodesList from '~/components/episodes/EpisodesList'
 import CelebsCast from '~/components/series/CelebsCast'
 import { getSerieByPath } from '~/assets/js/api'
 import Reviews from '~/components/reviews/Reviews'
+import SerieTrailors from '~/components/series/SerieTrailors'
 
 export default {
-  components: { RatingCard, EpisodesList, CelebsCast, Reviews },
+  components: { RatingCard, EpisodesList, CelebsCast, Reviews, SerieTrailors },
   data () {
     return {
       baseUrl: process.env.baseUrl
@@ -105,7 +108,6 @@ export default {
   padding: 0 0 25px 25px;
   width: 300px;
 }
-
 .card-media-img {
   box-shadow: 0 4px 6px rgba(0, 0, 0, .3);
   margin-top: -50px;
