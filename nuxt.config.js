59a4590949933c72013709393550081e07440ac2
Ballmdr <ball.mdr@gmail.com>
2018-10-27 21:22:51 +0700

    celeb

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.4.2/css/all.css' }
     ]
   },
   plugins: [ 
