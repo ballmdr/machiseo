350e9ae22f3cb25e8d3eee9f8d91c67530577143
Ballmdr <ball.mdr@gmail.com>
2018-11-04 08:58:47 +0700

    google

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' },
+      { name: "google-site-verification", content: "rgWusjEPsoA8fiiuTVzNWFsAVOxl7_Om0OVztVuFnwc" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
