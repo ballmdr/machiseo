93ce307138268c259ba3e3c7a0dd368cad7161b3
Ballmdr <ball.mdr@gmail.com>
2018-11-04 09:15:19 +0700

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'machiseo',
+    title: 'มาชิสซอ machiseo.com',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
