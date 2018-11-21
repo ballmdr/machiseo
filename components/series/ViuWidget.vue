4e2956bb696a79fd50d7e65378a845d5673523f1
Ballmdr <ball.mdr@gmail.com>
2018-11-21 10:27:39 +0700

    fix html

--- components/series/ViuWidget.vue
+++ components/series/ViuWidget.vue
@@ -1,21 +1,28 @@
 <template>
-  <iframe 
-  :src="'https://www.viu.com/ott/misc/widget/index.php?product_id=' + vid_id + '&event=th30&utm_source=machiseo&utm_campaign=widget&utm_medium=vdowidget&utm_content=' + serie_title" 
-  width="70%" style="max-width:600px" height="400" frameborder="0" allowfullscreen="true" scrolling="no" 
-  ><p>Your browser does not support iframes.</p>
-  </iframe>
+  <div style="margin:auto;max-width:600px;">
+    <iframe :src="widgetSrc" frameborder="0" width="80%" height="300"></iframe>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['vid_id', 'serie_title']
+  props: ['vid_id', 'serie_title'],
+  data () {
+    return {
+      widgetSrc: '',
+      widget: ''
+    }
+  },
+  mounted () {
+    this.widgetSrc = 'https://www.viu.com/ott/misc/widget/index.php?product_id=' + this.vid_id + '&event=th30&utm_source=machiseo&utm_campaign=widget&utm_medium=vdowidget&utm_content=' + this.serie_title
+  }
 }
 </script>
 
 <style scoped>
 @media only screen and (max-width: 600px) {
   iframe {
-    height:200px;
+    height:220px;
   }
 } 
 </style>
