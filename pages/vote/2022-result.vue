c9d7e071388b58737a14e75d8e83ad4da16e4f30
thana.lee <ball.mdr@gmail.com>
2022-11-29 14:08:31 +0700

    final

--- pages/vote/2022-result.vue
+++ pages/vote/2022-result.vue
@@ -40,7 +40,7 @@
 </template>
   
 <script>
-  
+
   function getRandomColor () {
     const letters = '0123456789ABCDEF'
     let color = '#'
@@ -77,12 +77,12 @@
           { hid: 'og_type', name: 'og:type', content: 'article' },
           { hid: 'og_title', name: 'og:title', content: 'ผลโหวตล่าสุด ซีรีส์เกาหลีแห่งปี 2022' },
           { hid: 'og_description', name: 'og:description', content: synopsis },
-          { hid: 'og_image', name: 'og:image', content: 'https://machiseo.net/sites/default/files/vote-2018.jpg' },
+          { hid: 'og_image', name: 'og:image', content: 'https://www.machiseo.net/sites/default/files/vote-2022.jpg' },
           { hid: 'og_url', name: 'og:url', content: canonical },
           { hid: 'og_sitename', name: 'og:site_name', content: 'มาชิสซอ Machiseo.com' },
           { hid: 'twitter_title', name: 'twitter:title', content: 'ผลโหวตล่าสุด ซีรีส์เกาหลีแห่งปี 2022' },
           { hid: 'twitter_description', name: 'twitter:description', content: synopsis },
-          { hid: 'twitter_image', name: 'twitter:image', content: 'https://machiseo.net/sites/default/files/vote-2018.jpg' },
+          { hid: 'twitter_image', name: 'twitter:image', content: 'https://www.machiseo.net/sites/default/files/vote-2022.jpg' },
           { hid: 'twitter_site', name: 'twitter:site', content: '@machiseo' },
           { hid: 'twitter_creator', name: 'twitter:creator', content: '@machiseo' }
         ],
